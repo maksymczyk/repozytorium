00f244a7f6d8177bb87296a325a9af3cc2d56d9a
maksy <maksymczyk77@gmail.com>
2018-12-21 14:34:59 +0100

    pozdro

--- both.py
+++ both.py
@@ -5,34 +5,39 @@ from scipy import stats
 
 dane=pd.read_csv("c:/Users/maksy/both_sexes.csv")
 
-zmiennaWszyscy = 1-dane["all_2534"]
-zmiennaWszyscy = zmiennaWszyscy.fillna(0)
-zmiennaLiceum = 1-dane["HS_2534"]
-zmiennaLiceum = zmiennaLiceum.fillna(0)
-zmiennaCollege = 1-dane["SC_2534"]
-zmiennaCollege = zmiennaCollege.fillna(0)
-zmiennaLicencjat = 1-dane["BAp_2534"]
-zmiennaLicencjat - zmiennaLicencjat.fillna(0)
-zmiennaMagister = 1-dane["GD_2534"]
-zmiennaMagister = zmiennaMagister.fillna(0)
-zmiennaBiali = 1-dane["White_2534"]
-zmiennaBiali = zmiennaBiali.fillna(0)
-zmiennaCzarni = 1-dane["Black_2534"]
-zmiennaCzarni = zmiennaCzarni.fillna(0)
-zmiennaBiedni = 1-dane["poor_2534"]
-zmiennaBiedni = zmiennaBiedni.fillna(0)
-zmiennaSredni = 1-dane["mid_2534"]
-zmiennaSredni = zmiennaSredni.fillna(0)
-zmiennaBogaci = 1-dane["rich_2534"]
-zmiennaBogaci = zmiennaBogaci.fillna(0)
-zmiennaDzieci = 1-dane["kids_all_2534"]
-zmiennaDzieci = zmiennaDzieci.fillna(0)
-zmiennaBrakdzieci = 1-dane["nokids_all_2534"]
-zmiennaBrakdzieci = zmiennaBrakdzieci.fillna(0)
-zmiennaRok = 1-dane["year"]
-zmiennaRok = zmiennaRok.fillna(0)
+zmiennaWszyscy = 1 - dane["all_2534"]
+Wszyscy = zmiennaWszyscy.mean()
+zmiennaWszyscy = zmiennaWszyscy.fillna(Wszyscy)
+zmiennaDyplom = 1 - dane["GD_2534"]
+Dyplom = zmiennaDyplom.mean()
+zmiennaDyplom = zmiennaDyplom.fillna(Dyplom)
+zmiennaLiceum = 1 - dane["HS_2534"]
+Liceum = zmiennaLiceum.mean()
+zmiennaLiceum = zmiennaLiceum.fillna(Liceum)
+zmiennaBiali = 1 - dane["White_2534"]
+Biali = zmiennaBiali.mean()
+zmiennaBiali = zmiennaBiali.fillna(Biali)
+zmiennaCzarni = 1 - dane["Black_2534"]
+Czarni = zmiennaCzarni.mean()
+zmiennaCzarni = zmiennaCzarni.fillna(Czarni)
+zmiennaBiedni = 1 - dane["poor_2534"]
+Biedni = zmiennaBiedni.mean()
+zmiennaBiedni = zmiennaBiedni.fillna(Biedni)
+zmiennaSredni = 1 - dane["mid_2534"]
+Sredni = zmiennaSredni.mean()
+zmiennaSredni = zmiennaSredni.fillna(Sredni)
+zmiennaBogaci = 1 - dane["rich_2534"]
+Bogaci = zmiennaBogaci.mean()
+zmiennaBogaci = zmiennaBogaci.fillna(Bogaci)
+zmiennaDzieci = 1 - dane["kids_all_2534"]
+Dzieci = zmiennaDzieci.mean()
+zmiennaDzieci = zmiennaDzieci.fillna(Dzieci)
+zmiennaBrakdzieci = 1 - dane["nokids_all_2534"]
+Brakdzieci = zmiennaBrakdzieci.mean()
+zmiennaBrakdzieci = zmiennaBrakdzieci.fillna(Brakdzieci)
+zmiennaRok = dane["year"]
 
-#test różnic w małżeństwach miedzy białymi i czarnymi osobami (25-34 lata)
+#test różnic w małżeństwach miedzy białymi i czarnymi osobami (
 print(stats.shapiro(zmiennaCzarni))
 print(stats.shapiro(zmiennaBiali))
 
@@ -49,7 +54,9 @@ stdzmiennaBiali= pd.Series(zmiennaBiali).std()
 stdludzie = pd.Series([ stdzmiennaCzarni, stdzmiennaBiali])
 plt.bar(range(len(ludzie)), ludzie, color='g', label="odsetek małżeństw" , yerr=stdludzie , width=0.5, align='center')
 plt.xticks(range(len(ludzie)) , ['osoby czarnoskóre', 'osoby białoskóre'])
-plt.ylabel("odsetek małżeństw")
+ax = plt.gca()
+ax.tick_params(labelsize = 14)
+plt.ylabel("odsetek małżeństw [%]", size=14)
 plt.show()
 
 #test t dla wszystkich
@@ -61,10 +68,13 @@ print(testt1)
 #wykres wskażników opisujących jak zmieniała sie liczba małżeństw na przestrzeni lat 1960-2012
 
 lzmiennaWszyscy=pd.Series(zmiennaWszyscy)
+lzmiennaWszyscy=lzmiennaWszyscy*100
 plt.plot(lzmiennaWszyscy, label="odsetek małżeństw")
 plt.legend()
 plt.xlabel("lata")
-plt.ylabel("odsetek małżeństw")
+plt.ylabel("odsetek małżeństw [%]")
+y_pos = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17] # Układa kolumny w równych odstępach
+plt.xticks(y_pos, zmiennaRok, rotation = 'vertical')
 plt.show()
 
 #test t da bezdzietnych
@@ -75,8 +85,11 @@ print(testt2)
 #wykres wskażników opisujących jak zmieniała sie liczba małżeństw bezdzietnych na przestrzeni lat 1960-2012
 
 lzmiennaBrakdzieci=pd.Series(zmiennaBrakdzieci)
+lzmiennaBrakdzieci=lzmiennaBrakdzieci*100
 plt.plot(lzmiennaBrakdzieci, label="odsetek małżeństw bezdzietnych")
 plt.legend()
 plt.xlabel("lata")
-plt.ylabel("odsetek małżeństw bezdzietnych")
+plt.ylabel("odsetek małżeństw bezdzietnych [%]")
+y_pos2 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+plt.xticks(y_pos2, zmiennaRok, rotation = 'vertical')
 plt.show()
